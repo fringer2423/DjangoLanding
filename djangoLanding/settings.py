953a54d9081fc5b79030a819689bdc085435dcb7
Vladislav <admin@tech-hunter.ru>
2022-04-27 16:37:05 +0300

    add config

--- djangoLanding/settings.py
+++ djangoLanding/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import local_settings
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wgu=(mjza7vrv(d-kku#l6kc$m4x!08k6lr$ku+8=1z4r!+i*-'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
